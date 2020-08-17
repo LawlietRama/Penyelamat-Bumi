c045578ba3984b43053fe92fad4c8ad224d5ff20
LawlietRama <auliarama3499@gmail.com>
2020-08-17 23:42:50 +0700

    YOU CAN NOW CHANGE CAMERA ANGLE IN CERTAIN AREAS!
    Sesuai judul

---
+++ Assets/Scripts/CameraChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChanger : MonoBehaviour
+{
+    public GameObject camBefore;
+    public GameObject camAfter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            camBefore.SetActive(false);
+            camAfter.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        camBefore.SetActive(true);
+        camAfter.SetActive(false);
+    }
+}
