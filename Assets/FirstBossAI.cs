8438c00268988e8a13f861918a447f66be6adae7
LawlietRama <auliarama3499@gmail.com>
2020-10-08 12:06:35 +0700

    first boss can chase player check

---
+++ Assets/FirstBossAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FirstBossAI : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(new Vector3(transform.position.x, transform.position.y, transform.position.z), new Vector3(target.position.x, transform.position.y, target.position.z), step);
+    }
+}
